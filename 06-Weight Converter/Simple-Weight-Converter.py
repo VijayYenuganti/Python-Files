07aae889628bccc954a7701a963c5900570794fa
Yenuganti Vijay Kumar <yenugantivijaykumar@gmail.com>
2024-08-15 16:13:29 +0530

    Update Simple-Weight-Converter.py

--- 06-Weight Converter/Simple-Weight-Converter.py
+++ 06-Weight Converter/Simple-Weight-Converter.py
@@ -1,16 +1,19 @@
 # Weight Converter.
 
 weight = float(input("Enter Your Weight > "))
-unit = input("To which unit you want to convert (K or L) > ").upper()
+unit = input("To which unit you want to convert (K or L) > ")
+
+unit.strip().upper()
 
 if unit == "K":
     weight /= 2.205
-    unit = "Kgs."
-    print(f"Your weight is: {round(weight, 2)} {unit}")
+    units = "Kgs."
+    print(f"Your weight is: {round(weight, 2)} {units}")
 elif unit == "L":
     weight *= 2.205
-    unit = "Lbs."
-    print(f"Your weight is: {round(weight, 2)} {unit}")
+    units = "Lbs."
+    print(f"Your weight is: {round(weight, 2)} {units}")
 else:
-    print(f"Enter Valid Units.")
+    print(f"{unit} Enter Valid Units.")
+
 
